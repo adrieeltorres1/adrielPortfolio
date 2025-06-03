b7d0e02b9d73e90b5b13fa0c2c628007f1bd19a7
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-06-03 07:08:30 -0300

    Estilizando componente Experiência

---
+++ src/components/Experiencias.jsx
@@ -0,0 +1,146 @@
+import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { BsHeadset, BsTools } from "react-icons/bs";
+
+const Experiencias = () => {
+    return (
+        <>
+            <h2 className="flex justify-center font-semibold text-4xl text-white">Experiências</h2>
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{
+                        background: '#212121',
+                        color: '#fff',
+                        borderBottom: '2px solid #1ed760',
+                        boxShadow: 'none'
+                    }}
+                    contentArrowStyle={{ borderRight: '7px solid  #212121' }}
+                    date="2025 - Atual"
+                    iconStyle={{
+                        background: '#1ed760',
+                        color: '#fff',
+                        boxShadow: 'none'
+                    }}
+                    icon={<BsTools className="text-[#212121]" />}
+                >
+                    <h3 className="vertical-timeline-element-title text-2xl font-semibold">Analista De Suporte N1</h3>
+                    <h4 className="vertical-timeline-element-subtitle italic text-[#b3b3bb] text-sm">GIGA MAIS FIBRA</h4>
+                    <div class="flex flex-wrap gap-3 mt-3 font-medium text-black">
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Comunicação
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Pensamento Crítico
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Resolução de Problemas
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Paciência
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Proatividade
+                        </div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ad quis ea odio est laborum porro labore neque quasi eius, aliquid itaque. Facere dolore sint asperiores consequuntur perspiciatis accusamus porro dolorum autem
+                    </p>
+                    <p>
+                        minus exercitationem reiciendis unde nisi animi natus soluta temporibus sequi voluptate sed commodi, excepturi tempore voluptas quo iusto tenetur?
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{
+                        background: '#212121',
+                        color: '#fff',
+                        borderBottom: '2px solid #1ed760',
+                        boxShadow: 'none'
+                    }}
+                    contentArrowStyle={{ borderRight: '7px solid  #212121' }}
+                    date="2024 - 2025"
+                    iconStyle={{
+                        background: '#1ed760',
+                        color: '#fff',
+                        boxShadow: 'none'
+                    }}
+                    icon={<BsHeadset className="text-[#212121]" />}
+                >
+                    <h3 className="vertical-timeline-element-title text-white text-2xl font-semibold">Assistente De Suporte Técnico</h3>
+                    <h4 className="vertical-timeline-element-subtitle italic text-[#b3b3bb] text-sm">GIGA MAIS FIBRA</h4>
+                    <div class="flex flex-wrap gap-3 mt-3 font-medium text-black">
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Comunicação
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Empatia
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Resolução de Problemas
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Paciência
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Proatividade
+                        </div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ad quis ea odio est laborum porro labore neque quasi eius, aliquid itaque. Facere dolore sint asperiores consequuntur perspiciatis accusamus porro dolorum autem
+                    </p>
+                    <p>
+                        minus exercitationem reiciendis unde nisi animi natus soluta temporibus sequi voluptate sed commodi, excepturi tempore voluptas quo iusto tenetur?
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{
+                        background: '#212121',
+                        color: '#fff',
+                        borderBottom: '2px solid #1ed760',
+                        boxShadow: 'none'
+                    }}
+                    contentArrowStyle={{ borderRight: '7px solid  #212121' }}
+                    date="2023 - 2024"
+                    iconStyle={{
+                        background: '#1ed760',
+                        color: '#fff',
+                        boxShadow: 'none'
+                    }}
+                    icon={<BsHeadset className="text-[#212121]" />}
+                >
+                    <h3 className="vertical-timeline-element-title text-white text-2xl font-semibold">Call Center</h3>
+                    <h4 className="vertical-timeline-element-subtitle italic text-[#b3b3bb] text-sm">NOTREDAME INTERMÉTICA HAPVIDA</h4>
+                    <div class="flex flex-wrap gap-3 mt-3 font-medium text-black">
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Comunicação
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Empatia
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Paciência
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Trabalho em equipe
+                        </div>
+                        <div class="bg-verde-spotify rounded-full px-4 py-1 flex justify-center items-center text-sm">
+                            Adaptabilidade
+                        </div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ad quis ea odio est laborum porro labore neque quasi eius, aliquid itaque. Facere dolore sint asperiores consequuntur perspiciatis accusamus porro dolorum autem
+                    </p>
+                    <p>
+                        minus exercitationem reiciendis unde nisi animi natus soluta temporibus sequi voluptate sed commodi, excepturi tempore voluptas quo iusto tenetur?
+                    </p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+
+        </>
+    );
+}
+
+export default Experiencias;
