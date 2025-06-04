9af1f2d546fb6ba20b40ce5b8977b3c0fab9d68a
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-06-04 04:34:25 -0300

    Criando e estilizando compnentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Experiencias from "./components/Experiencias";
+import Header from "./components/header";
 import './style.css';
 
 const App = () => {
   return (
     <>
+      <Header/>
       <Experiencias/>
     </>
   );
---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+        <>
+            <div className="text-white" >
+                <div>
+                    <h1>Adriel Torres</h1>
+                    <div className="flex gap-3"> 
+                        <div></div> //Instagram
+                        <div></div> //Linkedin
+                        <div></div> //Github
+                        <div></div> //WhatsApp
+                    </div>
+
+                    <p>Eu sou o Adriel, faço engenharia de software, Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eaque officiis id dignissimos assumenda eius recusandae reprehenderit nulla totam, cum aspernatur quas omnis quisquam. Unde cum totam quod omnis mollitia, repellat sed rem quia facere enim labore magnam facilis eaque velit consequuntur exercitationem maiores hic cupiditate accusantium autem atque porro harum. Sunt eius sed architecto quasi qui, a cupiditate! Molestias sed quibusdam numquam aperiam neque accusantium pariatur facere nostrum a enim aliquam esse temporibus excepturi, tempore voluptas ipsam qui, ipsa eius! Repudiandae praesentium cumque necessitatibus ut placeat autem dolores? Tenetur id illo minima non deserunt sed soluta. Quo, consectetur cupiditate.</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
