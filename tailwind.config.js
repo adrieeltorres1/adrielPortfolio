a4fe0d26b2ffcce2990263fd8a5602d87debdb29
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-06-07 01:44:16 -0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {
-       colors: {
-            clifford: '#da373d',
-            'verde-spotify': '#1ed760',
-          }
-     },
-   },
-   plugins: [],
- }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'verde-spotify': '#1ed760',
+        'verde-spotify': '#1ed760',
+        'laranja-html': '#E34F26',
+        'azul-css': '#1572B6',
+        'amarelo-javascript': '#F7DF1E',
+        'ciano-tailwind': '#06B6D4',
+        'azul-react': '#61DAFB',
+        'verde-node': '#339933',
+        'cinza-prisma': '#113b51',
+        'azul-postgresql': '#4169E1',
+        'verde-mongodb': '#47A248',
+        'vermelho-git': '#F05032'
+      }
+    },
+  },
+  plugins: [],
+}
